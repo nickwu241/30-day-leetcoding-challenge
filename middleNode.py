f9e9e7a935d596ec13ca6a8df4114b73f6013989
Nicholas Wu <nickwu241@gmail.com>
2020-04-09 03:30:56 -0700

    Create middleNode.py

---
+++ middleNode.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3290/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
